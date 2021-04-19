dd8ffc1db7611f0141116a70b9014ad90aaa7cec
emiln2002 <emilnoergaard2014@gmail.com>
2021-04-19 14:41:07 +0200

    multi Server er blevet lavet, der mangler komentare og den skal modifiseres til vores behov.

--- MultiBrugerServer.py
+++ MultiBrugerServer.py
@@ -1,39 +1,22 @@
-import selectors #for at kunne konekte til flere brugere af gangen
-import socket #for at lave forbindelsen mellem computer over IP
+#!/usr/bin/env python3
 
-FindHstename = socket.gethostname() #finder hostname
-host = socket.gethostbyname(FindHstename) #find ip'en
-port = 65432 #port den lytter på (skal være over 1024)
+import sys
+import socket
+import selectors
+import types
 
 sel = selectors.DefaultSelector()
 
 
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((host, port))
-lsock.listen()
-print('Lytter på', (host, port))
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
-#event loop
-while True:
-    events = sel.select(timeout=None)
-    for key, mask in events:
-        if key.data is None:
-            accept_wrapper(key.fileobj)
-        else:
-            service_connection(key, mask)
-#accept_wrapper() function
 def accept_wrapper(sock):
     conn, addr = sock.accept()  # Should be ready to read
-    print('accepted connection from', addr)
+    print("accepted connection from", addr)
     conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
     sel.register(conn, events, data=data)
 
-events = selectors.EVENT_READ | selectors.EVENT_WRITE #et objekt til at holde data indtil vi ved om clienter er klar til at læse og skrive
 
-#hvordan clienten er hånteret når den er conekted og klar
 def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
@@ -42,18 +25,38 @@ def service_connection(key, mask):
         if recv_data:
             data.outb += recv_data
         else:
-            print('closing connection to', data.addr)
+            print("closing connection to", data.addr)
             sel.unregister(sock)
             sock.close()
     if mask & selectors.EVENT_WRITE:
         if data.outb:
-            print('echoing', repr(data.outb), 'to', data.addr)
+            print("echoing", repr(data.outb), "to", data.addr)
             sent = sock.send(data.outb)  # Should be ready to write
             data.outb = data.outb[sent:]
-if recv_data:
-    data.outb += recv_data
-else:
-    print('closing connection to', data.addr)
-    sel.unregister(sock)
-    sock.close()
-data.outb = data.outb[sent:]
+
+
+
+
+FindHstename = socket.gethostname() #finder hostname
+host = socket.gethostbyname(FindHstename) #find ip'en
+port = 65432 #port den lytter på (skal være over 1024)
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print("listening on", (host, port))
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
